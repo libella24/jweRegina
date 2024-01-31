d25e4d62eea7740acbae84d135423a5c6d840ac9
libella24 <67199020+libella24@users.noreply.github.com>
2024-01-31 08:21:02 +0100

    Add files via upload

---
+++ cookies/js/cookies.js
@@ -0,0 +1,46 @@
+/*
+$('#set_cookie').click(function () {
+
+    let cookieValue = $('#cookie_value').val();
+    console.log(cookieValue);
+
+    if (cookieValue.length > 0) {
+        Cookies.set('jwe', cookieValue, { expires: 7 }); // Gültigkeit 7 Tage
+        console.log(`Cookie wurde mit dem Wert "${cookieValue}" erstellt.`);
+    } else {
+        console.warn('Sie haben keinen Wert für den Cookie angegeben!');
+    }
+});
+
+
+$('#read_cookie').click(function () {
+
+    // vom gelesenen Cookie bereitgestellter Wert wird in die Variable gespeichert
+    let cookieValue = Cookies.get('jwe');
+
+    // Übergabe des Wertes über die Variable ab das <div id="output"/>
+    $('#output').html(cookieValue);
+
+});
+*/
+
+
+$('#set_cookie').click(function(){
+
+    let cookieValue = $('#cookie_value').val();
+
+    if(cookieValue.length > 0) {
+        Cookies.set( 'jwe', cookieValue, { expires: 7 });
+        console.log(`Cookie wurde mit dem Wert ${cookieValue} erstellt.`);
+    } else {
+        console.warn('Sie haben keinen Wert für den Cookie angegeben!');
+    }
+
+});
+
+$('#read_cookie').click(function(){
+    // vom gelesenen Cookie bereitgestellter Wert wird in die Variable gespeichert
+    let cookieValue = Cookies.get('jwe');
+    // Übergabe des Wertes über die Variable an das <div id="output" />
+    $('#output').html(cookieValue);
+});
